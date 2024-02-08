6caf6cdaf86592c35ea71b0a1c69d6624ddd6b88
samir <samalam281@gmail.com>
2024-02-08 13:36:37 +0530

    bb

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express=require('express')
 const { connection } = require('./db')
 const { userRouter } = require('./Routes/users.Routes')
 const { courseRouter } = require('./Routes/course.Routes')
+const cors = require('cors')
 const app=express()
 app.use(express.json())
 app.use('/users',userRouter)
 app.use('/course',courseRouter)
+app.use(cors())
 app.listen(8080,async()=>{
     try{
         await connection
